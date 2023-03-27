ef61f9aee299951558bd1238b2347cc06f7bf803
Rodion Sigov <yellowboy911@bk.ru>
2023-03-27 20:36:06 +0400

    last

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import 'swiper/css/bundle';
 import './styles/main.scss';
 import './js/script';
 import 'custom-select';
-import IMask from 'imask';
+import 'accordion-js/dist/accordion.css';
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
+import IMask from 'imask';
 import Swiper from 'swiper/bundle';
+import Accordion from 'accordion-js';
+
 
 const swiperPopu = new Swiper(".popular", {
   spaceBetween: 18,
@@ -61,17 +64,17 @@ if (counter) {
 
 //----
 
-let btnPass = document.querySelector('.btn__pass');
-let inPass = document.querySelector('.password');
+let btnPass = document.querySelector('.input__glassIcon');
+let inPass = document.querySelector('.input__line-password');
 const checkbox = document.querySelector('.ui-checkbox')
 
 btnPass.onclick = function() {
   if (inPass.getAttribute('type') === 'password') {
     inPass.setAttribute('type', 'text');
-    btnPass.classList.add('act');
+    btnPass.classList.add('input__glassIcon--act');
   } else {
     inPass.setAttribute('type', 'password');
-    btnPass.classList.remove('act');
+    btnPass.classList.remove('input__glassIcon--act');
   }
 };
 
@@ -85,3 +88,45 @@ inPass.addEventListener('input', function() {
   }
 
 })
+
+
+//-----
+
+
+let phoneMask = IMask(
+  document.getElementById('phone-mask'), {
+    mask: '+{7}(000)000-00-00'
+  });
+
+
+//-----
+
+const buttons = document.querySelectorAll(".open-popup");
+const popup = document.querySelector(".popup");
+const buttonClose = popup.querySelector(".popup__close");
+const popupBlock = popup.querySelector(".popup__block");
+
+function openPopup() {
+  popup.classList.add("popup--active");
+}
+
+function closePopup() {
+  popup.classList.remove("popup--active");
+}
+
+buttons.forEach(function (item) {
+  item.addEventListener("click", openPopup);
+});
+
+buttonClose.addEventListener("click", closePopup);
+
+popup.addEventListener("click", closePopup);
+
+popupBlock.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+//----
+
+let accordion = new Accordion('.accordion__container');
+
