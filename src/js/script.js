6af82efd7f62398c74a00ab7087f2fc14f35ed67
Vladislav Iampolskii <cakefronend@gmail.com>
2023-03-27 22:17:59 +0400

    updates and fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,62 +1,60 @@
-import IMask from 'imask';
-import Swiper from 'swiper/bundle';
-import Accordion from 'accordion-js';
-
+import IMask from "imask"
+import Swiper from "swiper/bundle"
+import Accordion from "accordion-js"
 
 const swiperPopu = new Swiper(".popular", {
   spaceBetween: 18,
 
   breakpoints: {
-
     700: {
       slidesPerView: 2,
-      spaceBetween: 18,
+      spaceBetween: 18
     },
 
     1190: {
       slidesPerView: 3,
-      spaceBetween: 18,
+      spaceBetween: 18
     },
 
     1620: {
       slidesPerView: 4,
-      spaceBetween: 18,
-    },
-
+      spaceBetween: 18
+    }
   },
 
   scrollbar: {
     el: ".scrollbar-popular",
     draggable: true,
-    dragSize: 142,
-  },
-});
+    dragSize: 142
+  }
+})
 
 const swiperBaner = new Swiper(".baner-slider", {
   slidesPerView: 1,
   spaceBetween: 200,
   pagination: {
-    el: ".pagination-baner",
-  },
-});
+    el: ".pagination-baner"
+  }
+})
 
-
-const counter = document.querySelectorAll('[data-coun]');
+const counter = document.querySelectorAll("[data-coun]")
 if (counter) {
-  counter.forEach(counter => {
-    counter.addEventListener('click', e => {
-      const target = e.target;
-      if (target.closest('.counter__button')) {
-        let value = parseInt(target.closest('.counter').querySelector('input').value);
-        if (target.classList.contains('counter__butPlus')) {
-          value++;
+  counter.forEach((counter) => {
+    counter.addEventListener("click", (e) => {
+      const target = e.target
+      if (target.closest(".counter__button")) {
+        let value = parseInt(
+          target.closest(".counter").querySelector("input").value
+        )
+        if (target.classList.contains("counter__butPlus")) {
+          value++
         } else {
-          --value;
+          --value
         }
         if (value <= 0) {
-          value = 0;
+          value = 0
         }
-        target.closest('.counter').querySelector('input').value = value;
+        target.closest(".counter").querySelector("input").value = value
       }
     })
   })
@@ -64,69 +62,67 @@ if (counter) {
 
 //----
 
-let btnPass = document.querySelector('.input__glassIcon');
-let inPass = document.querySelector('.input__line-password');
-const checkbox = document.querySelector('.ui-checkbox')
+let btnPass = document.querySelector(".input__glassIcon")
+let inPass = document.querySelector(".input__line-password")
+const checkbox = document.querySelector(".ui-checkbox")
 
-btnPass.onclick = function() {
-  if (inPass.getAttribute('type') === 'password') {
-    inPass.setAttribute('type', 'text');
-    btnPass.classList.add('input__glassIcon--act');
-  } else {
-    inPass.setAttribute('type', 'password');
-    btnPass.classList.remove('input__glassIcon--act');
+if (btnPass) {
+  btnPass.onclick = function () {
+    if (inPass.getAttribute("type") === "password") {
+      inPass.setAttribute("type", "text")
+      btnPass.classList.add("input__glassIcon--act")
+    } else {
+      inPass.setAttribute("type", "password")
+      btnPass.classList.remove("input__glassIcon--act")
+    }
   }
-};
+}
 
-inPass.addEventListener('input', function() {
+inPass?.addEventListener("input", function () {
   if (this.value.length > 0) {
-    this.classList.remove('password--error')
-    this.classList.add('password--success')
+    this.classList.remove("password--error")
+    this.classList.add("password--success")
   } else {
-    this.classList.remove('password--success')
-    this.classList.add('password--error')
+    this.classList.remove("password--success")
+    this.classList.add("password--error")
   }
-
 })
 
-
 //-----
 
-
-let phoneMask = IMask(
-  document.getElementById('phone-mask'), {
-    mask: '+{7}(000)000-00-00'
-  });
-
+// let phoneMask = IMask(document.getElementById("phone-mask"), {
+//   mask: "+{7}(000)000-00-00"
+// })
 
 //-----
+const buttons = document.querySelectorAll(".open-popup")
+const popup = document.querySelector(".popup")
 
-const buttons = document.querySelectorAll(".open-popup");
-const popup = document.querySelector(".popup");
-const buttonClose = popup.querySelector(".popup__close");
-const popupBlock = popup.querySelector(".popup__block");
+if (popup) {
+  const buttonClose = popup.querySelector(".popup__close")
+  const popupBlock = popup.querySelector(".popup__block")
 
-function openPopup() {
-  popup.classList.add("popup--active");
+  function openPopup() {
+    popup.classList.add("popup--active")
+  }
+
+  function closePopup() {
+    popup.classList.remove("popup--active")
+  }
+
+  buttons.forEach(function (item) {
+    item.addEventListener("click", openPopup)
+  })
+
+  buttonClose.addEventListener("click", closePopup)
+
+  popup.addEventListener("click", closePopup)
+
+  popupBlock.addEventListener("click", (event) => {
+    event.stopPropagation()
+  })
 }
-
-function closePopup() {
-  popup.classList.remove("popup--active");
-}
-
-buttons.forEach(function (item) {
-  item.addEventListener("click", openPopup);
-});
-
-buttonClose.addEventListener("click", closePopup);
-
-popup.addEventListener("click", closePopup);
-
-popupBlock.addEventListener("click", (event) => {
-  event.stopPropagation();
-});
 
 //----
 
-let accordion = new Accordion('.accordion__container');
-
+let accordion = new Accordion(".accordion__container")
